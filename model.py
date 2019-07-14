a872b65a65560d56dadf99bec3d146b0ec229036
Wenbo <huwenbodut@gmail.com>
2019-07-14 17:43:30 +0800

    add model definition

---
+++ model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+    File Name   :   DEMC-model
+    date        :   14/7/2019
+    Author      :   wenbo
+    Email       :   huwenbodut@gmail.com
+    Description :
+                              _     _
+                             ( |---/ )
+                              ) . . (
+________________________,--._(___Y___)_,--._______________________
+                        `--'           `--'
+"""
+
